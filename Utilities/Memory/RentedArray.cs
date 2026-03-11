140c1182ed3ecbc4ac8213eb713b9a808d6863dc
Dmytro Galytskyi <galitskiy.dima8@gmail.com>
2026-03-11 22:46:57 +0100

    full rework of the RentedArray.cs

--- Utilities/Memory/RentedArray.cs
+++ Utilities/Memory/RentedArray.cs
@@ -3,31 +3,12 @@ using System.Collections;
 
 namespace Syncie.Utilities.Memory;
 
-public sealed class RentedArray<T> : ICollection, IMemoryOwner<T>, IList<T>, IDisposable
+public sealed class RentedArray<T> : IMemoryOwner<T>
 {
     private readonly T[] _array;
 
     public int Length { get; }
-    public int Count => Length;
-    public bool IsSynchronized => _array.IsSynchronized; // Note: Always false.
-    public object SyncRoot => _array.SyncRoot;
-    public bool IsReadOnly => false;
     public Memory<T> Memory => AsMemory();
-
-    public T this[int index]
-    {
-        get
-        {
-            ThrowIfIndexOutOfRange(index);
-            return _array[index];
-        }
-
-        set
-        {
-            ThrowIfIndexOutOfRange(index);
-            _array[index] = value;
-        }
-    }
 
     /// <summary>
     /// Creates an array that is rented from an <see cref="ArrayPool{T}"/>
@@ -39,69 +20,16 @@ public sealed class RentedArray<T> : ICollection, IMemoryOwner<T>, IList<T>, IDi
         Length = length;
     }
 
-    public Memory<T> AsMemory() => new(_array, 0, Count);
+    public Memory<T> AsMemory() => new(_array, 0, Length);
 
     public Memory<T> AsMemory(int length) => new(_array, 0, length);
-
-    public void Add(T item) => throw new NotSupportedException();
-
-    public void Clear() => ((IList<T>)_array).Clear();
-
-    public bool Contains(T item) => _array.Contains(item);
-
-    public bool Remove(T item) => ((IList<T>)_array).Remove(item);
-
-    public int IndexOf(T item) => _array.IndexOf(item);
-
-    public void Insert(int index, T item) => ((IList<T>)_array).Insert(index, item);
-
-    public void RemoveAt(int index) => ((IList<T>)_array).RemoveAt(index);
-
-    public void CopyTo(T[] array, int arrayIndex) => _array.CopyTo(array, arrayIndex);
-
-    public void CopyTo(Array array, int index) => _array.CopyTo(array, index);
+    
+    public Span<T> AsSpan() => new(_array, 0, Length);
+    
+    public Span<T> AsSpan(int length) => new(_array, 0, length);
 
     public void Dispose()
     {
-        lock (_array)
-        {
-            ArrayPool<T>.Shared.Return(_array, clearArray: true);
-        }
-    }
-
-    private void ThrowIfIndexOutOfRange(int index)
-    {
-        if (index < 0 || index > Count)
-            throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range.");
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        return new Enumerator(this);
-    }
-
-    private class Enumerator(RentedArray<T> array) : IEnumerator<T>
-    {
-        private int _index = -1;
-
-        public bool MoveNext() => ++_index < array.Count;
-
-        public void Reset()
-        {
-            _index = -1;
-        }
-
-        object? IEnumerator.Current => array[_index];
-
-        T IEnumerator<T>.Current => array[_index];
-
-        public void Dispose()
-        {
-        }
+        ArrayPool<T>.Shared.Return(_array, clearArray: true);
     }
 }
